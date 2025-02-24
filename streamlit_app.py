2fb9c2506657864de39adc000423c04c2898e250
jeonghun <jeonghun9326@naver.com>
2025-02-24 10:48:36 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,13 +3,14 @@ import requests
 import streamlit as st
 from streamlit_app_HR import run_excel_analysis
 from streamlit_app_insurance import run_insurance_analysis
+from streamlit_app_merge import run_excel_merge
 
 # ✅ 기능 선택 UI
 def select_feature():
     """ Streamlit UI에서 사용자가 사용할 기능을 선택하는 함수 """
     return st.sidebar.selectbox(
         "📌 사용할 기능을 선택하세요",
-        ["엑셀 병합 및 인원 분석", "4대보험료 검증 시스템", "급여 업무 시스템", "채용 분석 시스템"]
+        ["단순엑셀병합", "엑셀 병합 및 인원 분석", "4대보험료 검증 시스템", "급여 업무 시스템", "채용 분석 시스템"]
     )
 
 # ✅ 기능 실행
@@ -18,6 +19,8 @@ def main():
 
     feature_option = select_feature()
 
+    if feature_option == "단순엑셀병합":
+        run_excel_merge()  # ✅ 단순 엑셀 병합 실행~
     if feature_option == "엑셀 병합 및 인원 분석":
         run_excel_analysis()  # ✅ 엑셀 병합 및 인원 분석 실행
     if feature_option == "4대보험료 검증 시스템":
